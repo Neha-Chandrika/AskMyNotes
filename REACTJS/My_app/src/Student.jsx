659d62f203833a0965e4fa645f1242988b678777
Neha-Chandrika <samvida1986@gmail.com>
2026-07-25 22:28:00 +0530

    reactjs of vite frame done in the class for the project

---
+++ REACTJS/My_app/src/Student.jsx
@@ -0,0 +1,11 @@
+function Student() {
+  return (
+    <div>
+      <h2>Student Details</h2>
+      <p>Name: Neha Chandrika</p>
+      <p>Course: AIML</p>
+    </div>
+  );
+}
+
+export default Student;
