13cb10d17c429dd073219e4ba18b99abf208b03b
Neha-Chandrika <samvida1986@gmail.com>
2026-07-27 23:24:38 +0530

    Display student details by passing an array of objects of students

--- REACTJS/My_app/src/App.jsx
+++ REACTJS/My_app/src/App.jsx
@@ -1,11 +1,38 @@
-import Student from "./Student";
+import Student from "./components/Student";
 
 function App() {
+  const students = [
+    {
+    name : "Neha",
+    course : "CSE",
+    year : "2nd Year",
+    skill : "CSS"
+  }, 
+  {
+    name : "Aparajita",
+    course : "CSM",
+    year : "2nd Year",
+    skill : "JS"
+  }, 
+  {
+    name : "Samvida",
+    course : "CSE",
+    year : "2nd Year",
+    skill : "HTML"
+  }, 
+  {
+    name : "Bhargavi",
+    course : "CSD",
+    year : "2nd Year",
+    skill : "Python"
+  },
+];
+  
   return (
     <div>
       <h1>College Portal</h1>
-
-      <Student />
+      
+      <Student students = {students} />
     </div>
   );
 }
